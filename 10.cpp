1e4b46a805e90fdb28bac9cddda284195b9af9ab
Suvansh <suvanshd27@gmail.com>
2023-12-11 17:14:36 +0530

    Indirect Recursion

---
+++ 10.cpp
@@ -0,0 +1,31 @@
+// Indirect recursion
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void funB(int n);
+
+void funA(int n)
+{
+  if (n > 0)
+  {
+    printf("%d\t", n);
+    funB(n - 1);
+  }
+}
+void funB(int n)
+{
+  if (n > 1)
+  {
+    printf("%d\t", n);
+    funA(n / 2);
+  }
+}
+int main()
+{
+  int x = 20;
+  funA(x);
+  return 0;
+}
