3d41d372a2fa1b14a2c57859c6c5969b6ebd0306
Suvansh <suvanshd27@gmail.com>
2023-12-16 05:44:59 +0530

    practice

---
+++ HackerRank/30 Days challenge/1.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  string s[10];
+  for (int i = 0; i < 10; i++)
+  {
+    cin >> s[i];
+  }
+  cout << "Hello, World." << endl;
+  for (int i = 0; i < 10; i++)
+  {
+    cout << s[i] << " ";
+  }
+
+  return 0;
+}
